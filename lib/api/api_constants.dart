13c2e63f5850fd9867be5008d8fdc63f89d50a30
aifrosals <a.ifrosals@gmail.com>
2022-02-07 00:16:42 +0500

    chance complete

--- lib/api/api_constants.dart
+++ lib/api/api_constants.dart
@@ -1,10 +1,10 @@
 class ApiConstants {
-  static const String domain = 'http://172.168.1.106:3000/';
-  static const String socketPoint = 'http://172.168.1.106:3000/';
+  // static const String domain = 'http://172.168.1.106:3000/';
+  // static const String socketPoint = 'http://172.168.1.106:3000/';
 
-  // static const String domain = 'https://desolate-sierra-90130.herokuapp.com/';
-  // static const String socketPoint =
-  //     'https://desolate-sierra-90130.herokuapp.com/';
+  static const String domain = 'https://desolate-sierra-90130.herokuapp.com/';
+  static const String socketPoint =
+      'https://desolate-sierra-90130.herokuapp.com/';
 
   static const String login = 'login';
   static const String slots = 'getSlots';
