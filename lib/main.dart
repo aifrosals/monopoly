6fc31630fac7708f287707562d05ffc54507072f
aifrosals <a.ifrosals@gmail.com>
2022-05-09 03:11:28 +0500

    removed admin app and transferred

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,15 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:monopoly/pages/start_page.dart';
-import 'package:monopoly/providers/admin_provider.dart';
-import 'package:monopoly/providers/admin_questions_provider.dart';
 import 'package:monopoly/providers/board_provider.dart';
 import 'package:monopoly/providers/dice_provider.dart';
 import 'package:monopoly/providers/transaction_provider.dart';
 import 'package:monopoly/providers/user_provider.dart';
-import 'package:monopoly/web/admin/admin_app.dart';
 import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'config/values.dart';
 
 void main() async {
-  if (kIsWeb) {
-    WidgetsFlutterBinding.ensureInitialized();
-    runApp(MultiProvider(providers: [
-      ChangeNotifierProvider<AdminProvider>(
-        create: (context) => AdminProvider(),
-      ),
-      ChangeNotifierProvider<AdminQuestionProvider>(
-        create: (context) => AdminQuestionProvider(),
-      ),
-    ], child: const AdminApp()));
-  } else {
     runApp(MultiProvider(providers: [
       ChangeNotifierProvider<UserProvider>(
         create: (context) => UserProvider(),
@@ -40,7 +25,6 @@ void main() async {
       ),
     ], child: const MyApp()));
   }
-}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
